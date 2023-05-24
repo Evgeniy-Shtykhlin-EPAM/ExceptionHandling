8a1cedbb15621b463ff24ef850f5a4a24f5f4c54
Evgeniy-Shtykhlin-EPAM <Evgeniy_Shtykhlin@epam.com>
2023-05-24 22:14:34 +0600

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            bool validateStr;
+            do
+            {
+                Console.WriteLine("Enter text:");
+                var str = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine("First symbol is: " + str.First());
+                    validateStr=true;
+                }
+                catch (InvalidOperationException)
+                {
+                    validateStr = false;
+                    Console.WriteLine("InvalidOperationException. Empty line");
+                }
+
+            } while (!validateStr);
+        }
+    }
+}
---
+++ Task2/NumberParser.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public int Parse(string stringValue)
+        {
+            if (stringValue == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (string.IsNullOrWhiteSpace(stringValue) || (stringValue.Contains("+-") || stringValue.Contains("-+") || stringValue.Contains("0-")))
+            {
+                throw new FormatException();
+            }
+            if (stringValue == "-2147483648")
+            {
+                return int.MinValue;
+            }
+
+            stringValue = stringValue.Replace(" ", "");
+            stringValue = stringValue.Replace("+", "");
+
+            int result = 0;
+            int multiple = 1;
+
+            if (stringValue[0] == 45)
+            {
+                stringValue = stringValue.Replace("-", "");
+                for (var i = stringValue.Length - 1; i >= 0; i--)
+                {
+                    int number;
+                    number = (int)((stringValue[i] - '0'));
+
+                    if ((int)stringValue[i] < 48 || (int)stringValue[i] > 57)
+                    {
+                        throw new FormatException();
+                    }
+                    else
+                    {
+                        result = checked(result + (number * multiple));
+                        multiple = multiple * 10;
+                    }
+                }
+                result = 0 - result;
+            }
+            else
+            {
+                for (var i = stringValue.Length - 1; i >= 0; i--)
+                {
+                    int number;
+                    number = (int)((stringValue[i] - '0'));
+
+                    if ((int)stringValue[i] < 48 || (int)stringValue[i] > 57)
+                    {
+                        throw new FormatException();
+                    }
+                    else
+                    {
+                        result = checked(result + (number * multiple));
+                        multiple = multiple * 10;
+                    }
+                }
+            }
+
+            if (result > int.MaxValue || result < int.MinValue)
+            {
+                throw new OverflowException();             
+            }
+
+            return result;
+        }
+    }    
+}
